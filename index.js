df05ea8b548ccca0d1cdb03f543f7b8cea67d435
Alex Gathungu <alex.gathungu@student.moringaschool.com>
2022-08-21 13:13:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName;
+function upperCaseCustomerName() {
+  String.toUpperCase(`${customerName}`);
+}
+
+var bestCustomer = 'not bob';
+
+function setBestCustomer() {
+  return bestCustomer;
+}
+
+function overWriteBestCustomer() {
+return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'bobby';
+function leastFavoriteCustomer() {
+  return leastFavoriteCustomer;
+}
